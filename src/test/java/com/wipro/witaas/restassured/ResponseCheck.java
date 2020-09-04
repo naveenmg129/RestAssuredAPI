1ee3f89704e8322f1ac337695a8a1a2b19534292
naveenmg129 <naveenmg129@gmail.com>
2020-09-04 13:53:18 +0530

    second code changes

--- src/test/java/com/wipro/witaas/restassured/ResponseCheck.java
+++ src/test/java/com/wipro/witaas/restassured/ResponseCheck.java
@@ -1,9 +1,22 @@
 package com.wipro.witaas.restassured;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
 public class ResponseCheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Validate if Add place API is working as expected
+		
+		// given - all input details
+		// when - Submit the api
+		// Then - validate the response
+		
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+//		given().queryParam("key", "qaclick123").header(headerName, headerValue, additionalHeaderValues)
+		System.out.println("Naveen");
 
 	}
 
